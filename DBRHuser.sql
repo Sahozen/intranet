1845bbc2f6448d9d3a43f3ec16bc2ae232dafec7
Saho <147002941+Sahozen@users.noreply.github.com>
2025-02-19 11:28:03 +0100

    Update DBRHuser.sql

--- DBRHuser.sql
+++ DBRHuser.sql
@@ -1,20 +1,22 @@
 CREATE TABLE import_users (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    GivenName NVARCHAR(50) NOT NULL,
-    Surname NVARCHAR(50) NOT NULL,
-    SamAccountName NVARCHAR(50) NOT NULL,
-    Department NVARCHAR(50) NOT NULL,
-    DisplayName NVARCHAR(100) NULL,
-    UserPrincipalName NVARCHAR(100) NULL,
-    EmailAddress NVARCHAR(100) NULL,
-    Title NVARCHAR(100) NULL,
-    TelephoneNumber NVARCHAR(50) NULL,
-    StreetAddress NVARCHAR(200) NULL,
-    POBox NVARCHAR(50) NULL,
-    PostalCode NVARCHAR(20) NULL,
-    StateOrProvince NVARCHAR(50) NULL,
-    Country NVARCHAR(10) NULL,
-    FullName NVARCHAR(100) NULL,
-    Initials NVARCHAR(10) NULL,
-    DateInserted DATETIME DEFAULT CURRENT_TIMESTAMP
-);
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  GivenName VARCHAR(50) NOT NULL,
+  Surname VARCHAR(50) NOT NULL,
+  SamAccountName VARCHAR(50) NOT NULL,
+  Department VARCHAR(50) NOT NULL,
+  DisplayName VARCHAR(100) NULL,
+  UserPrincipalName VARCHAR(100) NULL,
+  EmailAddress VARCHAR(100) NULL,
+  Title VARCHAR(100) NULL,
+  TelephoneNumber VARCHAR(50) NULL,
+  StreetAddress VARCHAR(200) NULL,
+  POBox VARCHAR(50) NULL,
+  PostalCode VARCHAR(20) NULL,
+  StateOrProvince VARCHAR(50) NULL,
+  Country VARCHAR(10) NULL,
+  FullName VARCHAR(100) NULL,
+  Initials VARCHAR(10) NULL,
+  DateInserted DATETIME DEFAULT CURRENT_TIMESTAMP
+)
+ENGINE=InnoDB
+DEFAULT CHARSET=utf8mb4;
